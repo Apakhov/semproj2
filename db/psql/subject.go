e95a2f3c8c108f76593d6a2c779138421eaa54f8
Mikhail Apakhov <m.apakhov@corp.mail.ru>
2020-07-22 03:30:59 +0300

    CRUD cathedra+subject

---
+++ db/psql/subject.go
@@ -0,0 +1,113 @@
+package psql
+
+import (
+	"errors"
+	"semprojdb/queryer"
+)
+
+type Subject struct {
+	ID        int64
+	ShortName string
+	FullName  string
+	CathedraID int64
+	IsDeleted bool
+}
+
+const sqlSubjectInsert = `
+INSERT INTO subject (short_name, full_name, cathedra_id) 
+VALUES ($1, $2, $3) RETURNING id;`
+
+func (db *DB) NewSubject(c *Subject) error {
+	ctx, cancel := defaultContext()
+	defer cancel()
+	err := db.conn.QueryRow(ctx, sqlSubjectInsert, c.ShortName, c.FullName, c.CathedraID).Scan(&c.ID)
+	if err != nil {
+		db.lg.Info("ERR1", err)
+		return err
+	}
+	return nil
+}
+
+func (db *DB) UpdSubject(f *Subject) error {
+	sql := queryer.Queryer{}
+	sql.WriteString("UPDATE subject SET")
+	sql.WithSep(",")
+	sql.IF(f.ShortName != "").
+		Sep().WriteStringArgs(" short_name = $$", f.ShortName)
+	sql.IF(f.FullName != "").
+		Sep().WriteStringArgs(" full_name = $$", f.FullName)
+	sql.IF(f.CathedraID != -1).
+		Sep().WriteStringArgs(" cathedra_id = $$", f.CathedraID)
+	sql.WriteStringArgs(" WHERE id = $$", f.ID)
+
+	ctx, cancel := defaultContext()
+	defer cancel()
+	_, err := db.conn.Exec(ctx, sql.String(), sql.Args()...)
+	if err != nil {
+		db.lg.Info("ERR1", err)
+		return err
+	}
+	return nil
+}
+
+func (db *DB) DelSubject(id int64, shName, flName string) error {
+	sql := queryer.Queryer{}
+	sql.WriteString("DELETE FROM subject WHERE").WithSep(" AND ")
+	sql.IF(shName != "").
+		Sep().WriteStringArgs(" short_name = $$", shName)
+	sql.IF(flName != "").
+		Sep().WriteStringArgs(" full_name = $$", flName)
+	sql.IF(id != -1).
+		Sep().WriteStringArgs(" id = $$", id)
+	if len(sql.Args()) == 0 {
+		return errors.New("cant delete nothing")
+	}
+
+	ctx, cancel := defaultContext()
+	defer cancel()
+	_, err := db.conn.Exec(ctx, sql.String(), sql.Args()...)
+	if err != nil {
+		db.lg.Info("ERR1", err)
+		return err
+	}
+	return nil
+}
+
+func (db *DB) GetSubjects(id int64, shName, flName string, fid int64, limit int64) ([]Subject, error) {
+	limit = fixLimit(limit, 50)
+	sql := queryer.Queryer{}
+	sql.WriteStringArgs(`
+		SELECT id, short_name, full_name, cathedra_id, deleted
+		FROM subject WHERE id >= $$`, id)
+	sql.
+		IF(shName != "").
+		WriteStringArgs(" AND short_name = $$", shName)
+	sql.
+		IF(flName != "").
+		WriteStringArgs(" AND full_name = $$", flName)
+	sql.
+		IF(fid != -1).
+		WriteStringArgs(" AND cathedra_id = $$", fid)
+	sql.WriteStringArgs(" ORDER BY id LIMIT $$", limit)
+
+	ctx, cancel := defaultContext()
+	defer cancel()
+	db.lg.Info(sql.String(), sql.Args())
+	rows, err := db.conn.Query(ctx, sql.String(), sql.Args()...)
+	if err != nil {
+		db.lg.Info("ERR1", err)
+		return nil, err
+	}
+	fs := make([]Subject, 0)
+	for cnt := 0; rows.Next(); cnt++ {
+		var f Subject
+		err := rows.Scan(&f.ID, &f.ShortName, &f.FullName, &f.CathedraID, &f.IsDeleted)
+		if err != nil {
+			db.lg.Info("ERR2", err)
+			return fs, err
+		}
+		fs = append(fs, f)
+	}
+	return fs, nil
+}
+
---
+++ handler/cathedra.go
@@ -0,0 +1,99 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"semprojdb/db/psql"
+	"semprojdb/helpers"
+)
+
+type Cathedra struct {
+	ID        int64  `json:"id"`
+	ShortName string `json:"short_name"`
+	FullName  string `json:"full_name"`
+	FacultyID int64  `json:"faculty_id"`
+	IsDeleted bool   `json:"is_deleted"`
+}
+
+func cathedraToDB(s *Cathedra, d *psql.Cathedra) {
+	d.ID = s.ID
+	d.ShortName = s.ShortName
+	d.FullName = s.FullName
+	d.FacultyID = s.FacultyID
+	d.IsDeleted = s.IsDeleted
+}
+
+func cathedraFromDB(s *psql.Cathedra, d *Cathedra) {
+	d.ID = s.ID
+	d.ShortName = s.ShortName
+	d.FullName = s.FullName
+	d.FacultyID = s.FacultyID
+	d.IsDeleted = s.IsDeleted
+}
+
+func (h *Handler) HandleCathedraCreate(w http.ResponseWriter, r *http.Request) {
+	f := new(Cathedra)
+	if !helpers.DecodeJSONBody(h.lg, w, r, f) {
+		return
+	}
+	fdb := new(psql.Cathedra)
+	cathedraToDB(f, fdb)
+	err := h.db.NewCathedra(fdb)
+	cathedraFromDB(fdb, f)
+	bt, _ := json.Marshal(WithError{
+		Err: psql.AnalizeDBError(err),
+		Val: fdb,
+	})
+	w.Write(bt)
+}
+
+func (h *Handler) HandleCathedraUpdate(w http.ResponseWriter, r *http.Request) {
+	f := new(Cathedra)
+	if !helpers.DecodeJSONBody(h.lg, w, r, f) {
+		return
+	}
+	fdb := new(psql.Cathedra)
+	cathedraToDB(f, fdb)
+	err := h.db.UpdCathedra(fdb)
+	cathedraFromDB(fdb, f)
+	bt, _ := json.Marshal(WithError{
+		Err: psql.AnalizeDBError(err),
+		Val: fdb,
+	})
+	w.Write(bt)
+}
+
+func (h *Handler) HandleCathedraRead(w http.ResponseWriter, r *http.Request) {
+	id, _ := helpers.ReadGetInt64(r, "id")
+	limit, lok := helpers.ReadGetInt64(r, "limit")
+	if !lok {
+		limit = 1
+	}
+	sh, _ := helpers.ReadGetString(r, "short_name")
+	fl, _ := helpers.ReadGetString(r, "full_name")
+	fid, _ := helpers.ReadGetInt64(r, "faculty_id")
+	h.lg.Info(id, sh, fl, limit)
+	fdbs, err := h.db.GetCathedras(id, sh, fl, fid, limit)
+	h.lg.Info(fdbs)
+	fs := make([]Cathedra, len(fdbs))
+	for i := range fdbs {
+		cathedraFromDB(&fdbs[i], &fs[i])
+	}
+	bt, _ := json.Marshal(WithError{
+		Err: psql.AnalizeDBError(err),
+		Val: fs,
+	})
+	w.Write(bt)
+}
+
+func (h *Handler) HandleCathedraDelete(w http.ResponseWriter, r *http.Request) {
+	id, _ := helpers.ReadGetInt64(r, "id")
+	sh, _ := helpers.ReadGetString(r, "short_name")
+	fl, _ := helpers.ReadGetString(r, "full_name")
+	err := h.db.DelCathedra(id, sh, fl)
+	bt, _ := json.Marshal(WithError{
+		Err: psql.AnalizeDBError(err),
+		Val: nil,
+	})
+	w.Write(bt)
+}
